ef3c363df8686f2c6ff562b453e15b99265efb9e
michoj10 <micho10j@gmail.com>
2021-07-28 17:51:01 +0300

    sign up and sign in + what ward did in the session

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,25 +8,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
-    private EditText email;
-    private Button button;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
 
-        button= (Button) findViewById(R.id.register);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openchallenges();
-            }
-        });
+
+        };
     }
-    public void openchallenges(){
-        Intent intent= new Intent(this, challenges.class);
-        startActivity(intent);
-    }
-}
+
+
